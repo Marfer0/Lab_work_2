b0aef271180eb6d9aad5779b4794aafe6b2db795
Marfer0 <vadimka.marunov@gmail.com>
2025-11-27 17:00:41 +0300

    исправлен шаблон, добавлена функция поиска в файле

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import re
+import requests
+from bs4 import BeautifulSoup
+
 
 
 def menu():
     while True:
         print("Добро пожаловать в программу для поиска слов, состоящих из двух одинаковых частей\n")
         print("Выберите действие:\n(1) - нахождение слов в пользовательском вводе\n"
-              "(2) - нахождение слов по url\n"
-              "(3) - нахождение слов в файле\n")
+              "(2) - нахождение слов в файле по его пути\n")
         option = int(input())
         options(option)
 
@@ -15,17 +17,29 @@ def options(option):
         inp = input()
         search(inp)
     elif option == 2:
-        pass
-    elif option == 3:
-        pass
-    elif option not in range(1,4):
+        inp = input()
+        file_search(inp)
+    elif option not in range(1,3):
         print("неизвестная команда, введите цифру от 1 до 3")
         menu()
 
 def search(string):
     result = []
-    for item in string.lower().split():
+    string = string.lower()
+    string = re.split(r'[,\s.!?;]+', string)
+    for item in string:
         if (re.fullmatch(r'\b(\w{2,})\1\b', item)) is not None:
             result.append(item)
     print(result,'\n')
+
+def file_search(path):
+    result = []
+    file_path = path
+    with open(file_path, 'r', encoding='UTF8') as file:
+            text = file.read()
+    if len(text) > 0:
+        print(text,'\n')
+        search(text)
+    else:
+        print("Файл пуст или не может быть открыт.\n")
 menu()
