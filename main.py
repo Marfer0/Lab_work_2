4281dd8cbd6db1cf46a1dda17537b2e712ee6c08
Marfer0 <vadimka.marunov@gmail.com>
2025-11-27 15:51:23 +0300

    функция для поиска в пользовательском вводе

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
+import re
+
+
 def menu():
     while True:
         print("Добро пожаловать в программу для поиска слов, состоящих из двух одинаковых частей\n")
-        print("Выберите действие:(1) - нахождение слов в пользовательском вводе\n"
+        print("Выберите действие:\n(1) - нахождение слов в пользовательском вводе\n"
               "(2) - нахождение слов по url\n"
               "(3) - нахождение слов в файле\n")
         option = int(input())
-        return option
+        options(option)
 
 def options(option):
     if option == 1:
-        pass
+        inp = input()
+        search(inp)
     elif option == 2:
         pass
     elif option == 3:
         pass
-    else:
+    elif option not in range(1,4):
         print("неизвестная команда, введите цифру от 1 до 3")
         menu()
+
+def search(string):
+    result = []
+    for item in string.lower().split():
+        if (re.fullmatch(r'\b(\w{2,})\1\b', item)) is not None:
+            result.append(item)
+    print(result,'\n')
+menu()
