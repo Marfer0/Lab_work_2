b9af74caa7542b50d33bd7f776ccec06ce9e29d1
Marfer0 <vadimka.marunov@gmail.com>
2025-11-27 15:34:11 +0300

    обработчик ввода

--- main.py
+++ main.py
@@ -4,5 +4,16 @@ def menu():
         print("Выберите действие:(1) - нахождение слов в пользовательском вводе\n"
               "(2) - нахождение слов по url\n"
               "(3) - нахождение слов в файле\n")
+        option = int(input())
+        return option
 
-
+def options(option):
+    if option == 1:
+        pass
+    elif option == 2:
+        pass
+    elif option == 3:
+        pass
+    else:
+        print("неизвестная команда, введите цифру от 1 до 3")
+        menu()
